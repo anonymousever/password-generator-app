b7e669fdec1ae709fb67d3709c3932f4d2d2bb70
Sajjad Fathollahpour <xi.un.nu@gmail.com>
2024-05-21 20:31:48 +0330

    Add basic styles, utils and config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        orange: '#fb7c58',
+        red: '#f64a4a',
+        yellow: '#f8cd65',
+        gray: '#817d92',
+        'dark-gray': '#24232c',
+        'very-dark-gray': '#18171f',
+        'neon-green': '#a4ffaf',
+      },
+      backgroundImage: {
+        check: 'url("/images/icon-check.svg")',
+      },
+    },
   },
   plugins: [],
 }
